57a171625bf5176f4ade591cc004edd1331dc00c
henrychia12 <henry_chia21@hotmail.co.uk>
2019-01-17 17:38:28 +0000

    rest & repository

--- src/main/java/app.java
+++
@@ -1,8 +0,0 @@
-
-public class app {
-	
-	public void print() {
-		System.out.println("Hello world to the standard out");
-	}
-
-}
---
+++ src/main/java/com/qa/business/service/AccountService.java
@@ -0,0 +1,13 @@
+package com.qa.business.service;
+
+public interface AccountService {
+	
+	String getAllAccounts();
+	
+	String addAccount(String account);
+	
+	String deleteAccount(Long id);
+	
+	String updateAccount(Long id, String willUpdateAccount);
+
+}
